51c8389cb6f23cfdf4eb80fc9e41243460f1a0f3
MonaLisa <101475590+lma0kek0zavr@users.noreply.github.com>
2024-10-04 19:34:23 +0300

    app created

---
+++ src/main/java/com/learnspring/springstudents/SpringstudentsApplication.java
@@ -0,0 +1,12 @@
+package com.learnspring.springstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstudentsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/learnspring/springstudents/SpringstudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.springstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
